8d223e051f48b7ac79b1fb6a31b81222293552ad
Felipe Mendes <lfelipecmendes@icloud.com>
2022-05-31 14:02:47 -0300

    adding navbar complete

--- src/components/UserNavbar.tsx
+++ src/components/UserNavbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Navbar, Container} from 'react-bootstrap'
+import {Navbar, Container, Nav} from 'react-bootstrap'
 import * as Icon from 'react-bootstrap-icons'
 
 function UserNavbar() {
@@ -11,6 +11,14 @@ function UserNavbar() {
             <Icon.Cart4/>
             Gamashop
           </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+          <Navbar.Collapse className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#usuarios'>Usuarios</Nav.Link>
+            <Nav.Link href='#fornecedores'>Fornecedores</Nav.Link>
+            <Nav.Link href='#produtos'>Produtos</Nav.Link>
+            <Nav.Link href='#entregas'>Entregas</Nav.Link>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
